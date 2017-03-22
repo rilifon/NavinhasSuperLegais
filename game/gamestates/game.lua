5a6192add37f23609b64fc02d46de7369a7d92dc
rilifon <pudimdefrutas@gmail.com>
2017-03-22 11:59:27 -0300

    Fixed music beat counter
    -Fixed music beat counter to actually count beats
    -Added some enemy examples

--- game/gamestates/game.lua
+++ game/gamestates/game.lua
@@ -26,8 +26,9 @@ function state:enter()
 
 	Ship.create(1,1)
 
-	Enemy.create(15,1,1,10,{2,3,4,4.5,5,6,7,7.1,7.2,7.3,7.4,7.5,8,8.5,9})
-	Enemy.create(14,2,2,10,{2.5,3.5,4.5,5,5.5,6.5,7.5,8.1,8.2,8.3,8.4,8.5,9})
+	Enemy.create(15,1,1,20,{2,3,4,5,10,12,14,16,20,21})
+	Enemy.create(15,2,2,20,{6,7,8,9,11,13,15,17,20,21})
+	Enemy.create(13,3,0,22,{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17})
 
 
 end
@@ -56,7 +57,7 @@ function state:update(dt)
 	end
 
 	--Update music beat tracker
-	MUSIC_BEAT = MUSIC_BEAT + (dt*60/BPM_M)
+	MUSIC_BEAT = MUSIC_BEAT + (dt*BPM_M/60)
 
 	Util.updateDrawTable(dt)
 
